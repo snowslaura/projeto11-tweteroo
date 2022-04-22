70e6a0e21a009d29248f7ba567eb639038118cb3
Laura Neves <lauracmneves@gmail.com>
2022-04-22 12:58:48 -0300

    Bugfix: User not found - logica

--- back-end/index.js
+++ back-end/index.js
@@ -7,27 +7,31 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+app.get("/tweets", (req,res)=>{
+    const lastTen =  tweets.slice(-10);
+    res.send(lastTen);
+})
+
 app.post("/sign-up", (req,res) =>{
- const signInData = req.body;
- user.push(signInData);
- res.send("OK")
+    const signInData = req.body;
+    user.push(signInData);
+    res.send("OK")
 })
 
 app.post("/tweets", (req,res)=>{
-const tweetData = req.body;
-const userData = user.find(user => user.username === tweetData.username)
-tweetData.avatar =  userData.avatar
-tweets.push(tweetData);
-res.send("OK")
-})
-
-app.get("/tweets", (req,res)=>{
-const lastTen =  tweets.slice(-10);
-res.send(lastTen);
+    const tweetData = req.body;
+    const userData = user.find(user => user.username === tweetData.username)
+    tweetData.avatar =  userData.avatar
+    tweets.push(tweetData);
+    if (!userData){
+        res.send("User not found")
+        return 
+    }
+    res.send("OK")
 })
 
 app.listen(5000, ()=>{
-    console.log("Server is running on port 5000")
+    console.log("Server is running on: http://localhost:5000")
 })
 
 
